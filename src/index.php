3fc0b515cd4e4f526407f62a133f56606e729e98
JVIVO <jzeybel@myges.fr>
2023-01-11 11:27:00 +0100

    Change compose

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Test PHP">
+        <title>Test PHP</title>
+    </head>
+    <body>
+        <h1>Test PHP</h1>
+        <?php
+            phpinfo();
+        ?>
+    </body>
+</html>
