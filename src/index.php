cc1f3f92dcb36edeb501ac51a60d8d3bde34dd12
TheRealHoko <julien.zeybel@live.fr>
2023-03-20 01:21:19 +0100

    Update compose yml and modular db connection

--- src/index.php
+++ src/index.php
@@ -7,16 +7,6 @@
         <title>Test PHP</title>
     </head>
     <body>
-        <?php
-            $password = $_ENV["MYSQL_ROOT_PASSWORD"];
-
-            $pdo = new PDO('mysql:host=db;dbname=mysql', 'root', $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-            $query = $pdo->query('SHOW VARIABLES like "version"');
-
-            $row = $query->fetch();
-
-            echo 'MySQL version:' . $row['Value'];
-        ?>
+        <?php require_once "dbcontext.php" ?>
     </body>
 </html>
